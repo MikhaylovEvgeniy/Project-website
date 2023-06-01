a3c87e85f0ab2725a8935d8b8c88d75c4d95901f
MikhaylovEvgeniy <106879997+MikhaylovEvgeniy@users.noreply.github.com>
2023-06-01 20:50:34 +0700

    Add files via upload

---
+++ js/vouchers/ArabianEmirates/ArabianEmiratesCatalog.js
@@ -0,0 +1,59 @@
+const REGARA = [
+    {
+        descrip: '',
+        hotel: '',
+    },
+]; 
+
+const TEX = [
+    {
+        tex: 'Описание отеля',
+        what: '',
+    },
+];
+
+const TEXSYL = [
+    {
+        h: 'ПУТЕВКИ',
+    },
+    {
+        h: 'АРАБСКИЕ ЭМИРАТЫ',
+    },
+];
+
+const TABLNAZ = [
+    {
+        top: 'Название отеля',
+        right: '-',
+    },
+]; 
+
+const TABLSEC = [
+    {
+        leftsec: 'Количество дней',
+        rightsec: '-',
+    },
+    {
+        leftsec: 'Обслуживание',
+        rightsec: '-',
+    },
+
+    {
+        leftsec: 'Расстояние до моря',
+        rightsec: '-',
+    },
+    {
+        leftsec: 'Цена',
+        rightsec: '-',
+    },
+]; 
+
+const TABLDOPINF = [
+    {
+    },
+];
+ 
+const HOTEL = [
+    {
+    },
+];
