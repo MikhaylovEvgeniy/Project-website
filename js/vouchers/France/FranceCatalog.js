691f0aaed001f792e063db6151d0fc54d827e41a
MikhaylovEvgeniy <106879997+MikhaylovEvgeniy@users.noreply.github.com>
2023-06-01 20:51:09 +0700

    Add files via upload

---
+++ js/vouchers/France/FranceCatalog.js
@@ -0,0 +1,59 @@
+const REGARA = [
+    {
+        descrip: '',
+        hotel: '',
+    },
+]; 
+
+const HOTEL = [
+    {
+    },
+];
+
+const TEX = [
+    {
+        tex: 'Описание отеля',
+        what: '',
+    },
+];
+
+const TEXSYL = [
+    {
+        h: 'ПУТЕВКИ',
+    },
+    {
+        h: 'ФРАНЦИЯ',
+    },
+];
+
+const TABLNAZ = [
+    {
+        top: 'Название отеля',
+        right: '-',
+    },
+]; 
+
+const TABLSEC = [
+    {
+        leftsec: 'Количество дней',
+        rightsec: '-',
+    },
+    {
+        leftsec: 'Обслуживание',
+        rightsec: '-',
+    },
+
+    {
+        leftsec: 'Расстояние до моря',
+        rightsec: '-',
+    },
+    {
+        leftsec: 'Цена',
+        rightsec: '-',
+    },
+]; 
+
+const TABLDOPINF = [
+    {
+    },
+];
