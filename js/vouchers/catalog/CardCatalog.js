2b8e4cb531a730645adf91ab788ba378c01f622d
MikhaylovEvgeniy <106879997+MikhaylovEvgeniy@users.noreply.github.com>
2023-06-01 20:47:45 +0700

    Add files via upload

---
+++ js/vouchers/catalog/CardCatalog.js
@@ -0,0 +1,65 @@
+const CARD = [
+    {
+        id: '1',
+        name: 'Тайланд',
+        img: 'https://i.yapx.ru/WF7NL.jpg',
+        category: 'asia',
+        url: 'http://127.0.0.1:5503/vouchers/Thailand/index.html'
+    },
+    {
+        id: '2',
+        name: 'Египет',
+        img: 'https://i.yapx.ru/WD9XR.jpg',
+        category: 'africa',
+        url: 'http://127.0.0.1:5503/vouchers/Egypt/index.html'
+    },
+    {
+        id: '3',
+        name: 'Мальдивы',
+        img: 'https://i.yapx.ru/WD9XO.jpg',
+        category: 'asia',
+        url: 'http://127.0.0.1:5503/vouchers/Maldives/index.html'
+    },
+    {
+        id: '4',
+        name: 'Араб. Эмираты',
+        img: 'https://i.yapx.ru/WD9XQ.png',
+        category: 'africa',
+        url: 'http://127.0.0.1:5503/vouchers/ArabianEmirates/index.html'
+    },
+    {
+        id: '5',
+        name: 'Япония',
+        img: 'https://i.yapx.ru/WD9XU.png',
+        category: 'asia',
+        url: 'http://127.0.0.1:5503/vouchers/Japan/index.html'
+    },
+    {
+        id: '6',
+        name: 'Грузия',
+        img: 'https://i.yapx.ru/WD9XT.png',
+        category: 'europe',
+        url: 'http://127.0.0.1:5503/vouchers/Georgia/index.html'
+    },
+    {
+        id: '7',
+        name: 'Мадагаскар',
+        img: 'https://i.yapx.ru/WGFMv.jpg',
+        category: 'africa',
+        url: 'http://127.0.0.1:5503/vouchers/Madagaskar/index.html'
+    },
+    {
+        id: '8',
+        name: 'Франция',
+        img: 'https://i.yapx.ru/WGFQX.jpg',
+        category: 'europe',
+        url: 'http://127.0.0.1:5503/vouchers/France/index.html'
+    },
+    {
+        id: '9',
+        name: 'Греция',
+        img: 'https://i.yapx.ru/WGFSg.jpg',
+        category: 'europe',
+        url: 'http://127.0.0.1:5503/vouchers/Greece/index.html'
+    },
+];
